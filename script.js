a478bed377a9edb5a0f10a6e25b612818742d43f
Joseph <taiwojoseph2003@gmail.com>
2026-05-27 23:32:39 +0100

    J13 : validation formulaire JS, affichage dynamique

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ bouton.addEventListener("click", function(){
         bouton.textContent="Mode Sombre";
     }
 });
+
+//Recupérer le formulaire et la div de confirmation.
+const formulaire = document.querySelector("#contact-form");
+const confirmation = document.querySelector("#message-confirmation");
+
+// Ecouter l'envoi du formulaire.
+formulaire.addEventListener("submit", (event)=>{
+    //Empecher le rechargement de la page.
+    event.preventDefault();
+
+    //Recuperer ce que l'utilisateur a tapé dans les champs.
+    const nom = document.querySelector("#nom").value ;
+    const email = document.querySelector("#email").value ;
+
+    //Verifier que les champs ne sont pas vides.
+    if(nom === "" || email === ""){
+        confirmation.textContent = "Remplis tous les champs stp";
+        confirmation.style.color = "red";
+    }else {
+        //afficher le message de succcès.
+        confirmation.textContent = `Merci ${nom}! ton message a bien été envoyé. `;
+        confirmation.style.color = "green";
+
+        //vider le formulaire après confirmation.
+        formulaire.reset();
+    }
+});
