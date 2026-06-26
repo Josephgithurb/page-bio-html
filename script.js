c34069c7ee2b7bd69fa5e5caad5a61bdc15a3eb5
Joseph <taiwojoseph2003@gmail.com>
2026-06-26 14:17:28 +0100

    J18: smoot scrool et menu fermer + J19: Map Filter Destructuration - JS Express

--- script.js
+++ script.js
@@ -88,3 +88,54 @@ const observer = new IntersectionObserver((entries)=> {
 document.querySelectorAll('.projet-item, .projet').forEach(el =>{
     observer.observe(el);
 }); 
+
+/* Fermer le menu burger apres clic sur un lien mobile */
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', ()=> {
+        document.querySelector('#menucheck').checked = false;
+    });
+});
+
+
+//J19: Map/ Filter/ Destructuration
+
+// Donnees de test = tes projets
+const Projets = [
+    {id: 1, nom: "Landing Page Bio", tech:"html", niveau: "debutant"},
+    {id: 2, nom: "Todo List", tech: "js", niveau: "intermediaire"},
+    {id: 3, nom: "Site Vitrine", tech:"html", niveau:"debutant"}
+];
+//1 MAP : Transforme chaque projet en HTML
+// En React  on fera exactement ca pour afficher une liste
+const nomsProjets = Projets.map(projet => projet.nom);
+console.log("Map noms:", nomsProjets);
+
+// Version React-ready : retourne du HTML
+const cartesHTML = Projets.map(projet => {
+    return `<div class="carte">${projet.nom} - ${projet.tech}</div>`;
+});
+console.log("Map HTML:", cartesHTML);
+
+//2. FILTER : Garde seulement les projets JS
+const projetsJS = Projets.filter(projet => projet.tech === "js");
+console.log("Filter JS:", projetsJS);
+
+//3. DESTRUCTURATION : Récupère direct ce qu'il faut
+const premierProjet = Projets[0];
+const {nom, tech} = premierProjet; //au lieu de const nom = premierProjet.nom
+console.log("Destructuré:", nom, tech);
+
+//4. COMBO MAP + DESTRUCTURATION = 90% du code React
+const listeReact = Projets.map(({id, nom, tech}) => {
+    return `${id}: ${nom} fait en ${tech}`;
+});
+console.log("Combo React:", listeReact);
+
+// Exercice 
+const nombres = [1, 2, 3, 4, 5];
+//1. Map : multiplie chaque nombre par 2
+const multiplienombres = nombres.map((nombre) => nombre*2);
+console.log("Resultat multiplication:", multiplienombres);
+//2. Filter : garde seulement les nombres > 6
+const multiplenombsup6 = multiplienombres.filter((nombre) => nombre > 6);
+console.log("Multiplication nombres >6 :", multiplenombsup6);
